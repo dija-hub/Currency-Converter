c6880777d4ba29b3aeff954c8e66374e188fa959
dija <dija5408@gmail.com>
2025-10-05 19:58:12 +0500

    Converter

--- currency.js
+++ currency.js
@@ -3,12 +3,29 @@ const BASE_URL =
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 
+
 for (let select of dropdowns) {   
   for (let currCode in countryList) {  
     let newOption = document.createElement("option");
     newOption.innerText = currCode;
     newOption.value = currCode;
+    if (select.name ==="from" && currCode ==="USD"){
+      newOption.selected="selected"
+    }
+    else if(select.name ==="to" && currCode ==="PKR"){
+      newOption.selected="selected";
+    }
     select.append(newOption);
   }
+  select.addEventListener("change",(evt)=>{
+     updateFlag(evt .target);
+  })
 }
+const updateFlag=(element)=>{
+      let currCode=element.value;
+      let countryCode=countryList[currCode];
+      let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
+      select.parentElement.querySelector("img");
+      img.src =newSrc;
 
+}
