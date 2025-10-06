0706a3a374dbc1c71009431186f4c07411610d08
dija <dija5408@gmail.com>
2025-10-06 19:10:53 +0500

    Converter

--- currency.js
+++ currency.js
@@ -32,19 +32,19 @@ const updateFlag=(element)=>{
       img.src =newSrc;
 }
 
-// btn.addEventListener("click",async(evt) =>{
-//   evt .preventDefault()
-// let ampount=document.querySelector(".amount input");
-// let amtVal=amount.value;
-// if (amtVal ==="" | amtVal <1){
-//   amtVal=1;
-//   amount.value="1";
-// }
+btn.addEventListener("click",async(evt) =>{
+  evt .preventDefault()
+let ampount=document.querySelector(".amount input");
+let amtVal=amount.value;
+if (amtVal ==="" | amtVal <1){
+  amtVal=1;
+  amount.value="1";
+}
 
-// const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-// let response= await fetch(URL)
-// console.log(response);
-// });
+const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+let response= await fetch(URL)
+console.log(response);
+});
 
 
 
