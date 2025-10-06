8365d99c00dec017903f1fcc4c4ca19e4eecb2b0
dija <dija5408@gmail.com>
2025-10-06 19:10:37 +0500

    Converter

--- currency.js
+++ currency.js
@@ -2,6 +2,10 @@ const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn=document.querySelector("form button")
+const fromCurr=document.querySelector(".from select")
+const toCurr=document.querySelector(".to select")
+
 
 for (let select of dropdowns) {   
   for (let currCode in countryList) {  
@@ -24,6 +28,26 @@ const updateFlag=(element)=>{
       let currCode=element.value;
       let countryCode=countryList[currCode];
       let newSrc=`https://flagsapi.com/${countryCode}/flat/64.png`;
-      select.parentElement.querySelector("img");
+      let img =element.parentElement.querySelector("img");
       img.src =newSrc;
 }
+
+// btn.addEventListener("click",async(evt) =>{
+//   evt .preventDefault()
+// let ampount=document.querySelector(".amount input");
+// let amtVal=amount.value;
+// if (amtVal ==="" | amtVal <1){
+//   amtVal=1;
+//   amount.value="1";
+// }
+
+// const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+// let response= await fetch(URL)
+// console.log(response);
+// });
+
+
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+btn.addEventListener("click",async(evt) =>{
+//   evt .preventDefault()
+// let ampount=document.querySelector(".amount input");
+// let amtVal=amount.value;
+// if (amtVal ==="" | amtVal <1){
+//   amtVal=1;
+//   amount.value="1";
+// }
+
+// const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+// let response= await fetch(URL)
+// console.log(response);
+// });
