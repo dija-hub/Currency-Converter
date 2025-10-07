07d1e39422f725c43bc8e6cea89967d8cd2e27b6
dija <dija5408@gmail.com>
2025-10-07 18:58:42 +0500

    Converter

--- currency.js
+++ currency.js
@@ -27,42 +27,42 @@ for (let select of dropdowns) {
 }
 
 
-// const updateFlag = (element) => {
-//   let currCode = element.value;
-//   let countryCode = countryList[currCode];
-//   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-//   let img = element.parentElement.querySelector("img");
-//   img.src = newSrc;
-// };
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
 
 
-// const updateExchangeRate = async () => {
-//   let amount = document.querySelector(".amount input");
-//   let amtVal = amount.value;
-//   if (amtVal === "" || amtVal < 1) {
-//     amtVal = 1;
-//     amount.value = "1";
-//   }
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
 
 
-//   const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
-//   let response = await fetch(URL);
-//   let data = await response.json();
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
 
-//   // Access nested currency value
-//   let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+  // Access nested currency value
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
 
-//   let finalAmount = (amtVal * rate).toFixed(2);
-//   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-// };
-
-
-// btn.addEventListener("click", (evt) => {
-//   evt.preventDefault();
-//   updateExchangeRate();
-// });
+  let finalAmount = (amtVal * rate).toFixed(2);
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
 
 
-// window.addEventListener("load", () => {
-//   updateExchangeRate();
-// });
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
